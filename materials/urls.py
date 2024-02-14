c65cfe402afe2597cebd1e0cabb4f964c68250f6
Andrey1989-G <goreglyad.andrey@gmail.com>
2024-02-14 13:48:50 +0500

    24.1 шаг 3 готово

---
+++ materials/urls.py
@@ -0,0 +1,20 @@
+from materials.apps import MaterialsConfig
+from rest_framework.routers import DefaultRouter
+
+from django.urls import path
+
+from materials.views import (CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView,
+                        LessonUpdateAPIView, LessonDestroyAPIView)
+
+app_name = MaterialsConfig.name
+
+router = DefaultRouter()
+router.register(r'course', CourseViewSet, basename='course')
+
+urlpatterns = [
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('lesson/list/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('lesson/view/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson_view'),
+    path('lesson/edit/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_edit'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+] + router.urls

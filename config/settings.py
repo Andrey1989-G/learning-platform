ba3641341986c338bd87644bf77b4a79adc00deb
Andrey1989-G <goreglyad.andrey@gmail.com>
2024-02-25 15:47:16 +0500

    26.1 Документирование и безопасность готово

--- config/settings.py
+++ config/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'django_filters',
     'payment',
     'rest_framework_simplejwt',
+    'corsheaders',
+    'drf_yasg',
 
 ]
 
@@ -62,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -191,3 +195,14 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
+
+# CORS_ALLOWED_ORIGINS = [
+#     '<http://localhost:8000>',  # Замените на адрес вашего фронтенд-сервера
+# ]
+#
+# CSRF_TRUSTED_ORIGINS = [
+#     "https://read-and-write.example.com", #  Замените на адрес вашего фронтенд-сервера
+#     # и добавьте адрес бэкенд-сервера
+# ]
+#
+# CORS_ALLOW_ALL_ORIGINS = False
--- payment/models.py
+++ payment/models.py
@@ -23,6 +23,11 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=15, decimal_places=2, verbose_name='сумма')
     pay_type = models.CharField(max_length=4, default=PAY_CARD, choices=PAY_TYPES, verbose_name='способ оплаты')
 
+    stripe_id = models.CharField(max_length=255, **NULLABLE, verbose_name='id оплаты')
+    stripe_status = models.CharField(max_length=10, **NULLABLE, verbose_name='статус оплаты')
+    stripe_url = models.CharField(max_length=500, **NULLABLE, verbose_name='ссылка на оплату')
+    is_paid = models.BooleanField(default=False, verbose_name='оплачено')
+
     def __str__(self):
         return f'{self.title}'
 
